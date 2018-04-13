f4d1297cade394e8accd51df59e8f56a60193980
Martin Mailhot <martin.mailhot@gmail.com>
2018-04-12 21:54:00 -0400

    ajout de fonction readdic pour rendre le code plus lisible

--- nomGen.py
+++ nomGen.py
@@ -4,11 +4,35 @@ from random import randint
 
 #pour avoir des options en ligne de commandes on utilise optparse.
 parser = OptionParser()
-parser.add_option("-n","--nombre", type="int", dest="num", help="NUM est egal au nombres de nom qu'on veut generer." )# on ajoute notre option, soit -n suivi d'un nombre pour modifier le nombre de sortie. par default il y a un seul nom.
+# on ajoute notre option, soit -n suivi d'un nombre pour modifier le nombre de sortie. par default il y a un seul nom.
+parser.add_option("-n","--nombre", type="int", dest="num", help="NUM est egal au nombres de nom qu'on veut generer." )
+
+# l'option -d pour ne pas avoir de double nom de famille (ou noms de famille compose)
 parser.add_option("-d", "--nodblnames", action="store_false", dest="dblname", help="BOOL si on ne veut pas de noms de famille compose on ajoute l'option -d")
-                  
+
 (options, args) = parser.parse_args()
 dblnamevar = options.dblname 
+
+#definition de variable
+
+dicnom = 'dictionnaires/nomFamille.txt'
+dicprenom = 'dictionnaires/prenom.txt'
+
+
+def readdic(dic):
+    "ceci ouvre le fichier dictionnaire si il est dans le bon sous-repertoire"
+    with open(dic) as fichier:
+        lignes = fichier.readlines()
+        lignes = [line.rstrip('\n') for line in open(dic)]
+        # determiner le plafond et le plancher pour la selection
+        # au hazard
+        plafond = len(lignes) #nombre de lignes total du fichier
+        plancher = int(plafond) - int(plafond) #le plancher c'est 0
+        plafond = plafond - 1 #l'index commence a 0 alors max - 1
+        hazard = randint(plancher,plafond)
+        sortie = lignes[hazard]
+    return sortie
+
 
 class prenomNom:
 
@@ -30,48 +54,28 @@ class prenomNom:
 
     def genPrenom(self):
         """ Cette fonction genere des prenoms en allant chercher une ligne au hazard dans le fichier dictionnaire """
-        with open('dictionnaires/prenom.txt') as fichier: #on ouvre le fichier
-            lignes = fichier.readlines() #on lit les lignes
-            lignes = [line.rstrip('\n') for line in open('dictionnaires/prenom.txt')]#on enleve le charactere de nouvelle ligne
-            # determiner le plafond et le plancher pour la selection
-            # au hazard
-            plafond = len(lignes) #nombre de lignes total du fichier
-            plancher = int(plafond) - int(plafond) #le plancher c'est 0
-            plafond = plafond - 1 #l'index commence a 0 alors max - 1
-            hazard = randint(plancher,plafond)#on trouve un nombre au hazard
-            sortie = lignes[hazard]#on va chercher le contenu de la ligne = au hazard
-        self.prenom = sortie #on assigne le prenom a la ligne trouve
+        self.prenom = str(readdic(dicprenom))
         return self.prenom #on retourne le resultat
 
     def genNom(self):
         """ Cette fonction genere des nom de famille en allant chercher une ligne au hazard dans le fichier dictionnaire """
-        with open('dictionnaires/nomFamille.txt') as fichier:
-            lignes = fichier.readlines()
-            lignes = [line.rstrip('\n') for line in open('dictionnaires/nomFamille.txt')]
-            # determiner le plafond et le plancher pour la selection
-            # au hazard
-            plafond = len(lignes) #nombre de lignes total du fichier
-            plancher = int(plafond) - int(plafond) #le plancher c'est 0
-            plafond = plafond - 1 #l'index commence a 0 alors max - 1
-            hazard = randint(plancher,plafond)
-            sortie = lignes[hazard]
-        self.nom = sortie
+        self.nom = str(readdic(dicnom))
         return self.nom
     def genDblNom(self):
         """ Cette fonction genere des nom de famille en allant chercher une ligne au hazard dans le fichier dictionnaire """
-        with open('dictionnaires/nomFamille.txt') as fichier:
-            lignes = fichier.readlines()
-            lignes = [line.rstrip('\n') for line in open('dictionnaires/nomFamille.txt')]
-            # determiner le plafond et le plancher pour la selection
-            # au hazard
-            plafond = len(lignes) #nombre de lignes total du fichier
-            plancher = int(plafond) - int(plafond) #le plancher c'est 0
-            plafond = plafond - 1 #l'index commence a 0 alors max - 1
-            hazard = randint(plancher,plafond)
-            sortie = lignes[hazard]
-        self.dblnom = sortie
+        self.dblnom = str(readdic(dicnom))
         return self.dblnom
-        
+
+
+# def multipletime(nbtime):
+#     count = 0
+#     while (count < options.num):
+#         leNom = prenomNom()
+#         print(str(leNom.result))
+#         count = count + 1    
+#     return
+
+    
 if options.num:
     count = 0
     while (count < options.num):
